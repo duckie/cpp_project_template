290ffe94692cdb93133b0398cf38e0c0333b35a8
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2016-07-09 01:07:17 +0200

    Change macro names

--- cmake/ProjectGlobalMacros.cmake
+++ cmake/ProjectGlobalMacros.cmake
@@ -1,9 +1,9 @@
 # Add 3rdparty for main file
-macro(enable_3rdparty name)
+macro(project_add_3rdparty name)
   add_subdirectory(3rdparty/${name})
-endmacro(enable_3rdparty)
+endmacro(project_add_3rdparty)
 
 # Add module for main file
-macro(enable_module name)
+macro(project_add_module name)
   add_subdirectory(src/${name})
-endmacro(enable_module)
+endmacro(project_add_module)
